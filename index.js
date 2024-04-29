4617058fe79a3bb019889c9ebacae9c6858acc6a
Devin <devin.everitt96@gmail.com>
2024-04-29 19:28:13 -0400

    created connection to database

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jimin199'
+    database: 'employees_db',
+});
