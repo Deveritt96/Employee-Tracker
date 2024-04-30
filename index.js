d2668387ad1b21ef5a603083bc738ab3594f7a2e
Devin <devin.everitt96@gmail.com>
2024-04-29 20:14:58 -0400

    Created menu

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Jimin199'
+    password: 'Jimin199',
     database: 'employees_db',
 });
+
+const menu = () => {
+    inquirer.prompt({
+        type: 'list',
+        message: 'Welcome',
+        choices: ['Display Departments',
+        'Display Roles', 
+        'Display Employees',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update Employee Role',
+        'Exit']
+    })
+}
